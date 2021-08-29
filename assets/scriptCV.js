9fc9508d188760e2b90dce6841210c8a3a6e770d
prtissot <priscilla.tissot@u-bordeaux.fr>
2021-08-29 11:58:19 +0200

    chat

--- assets/scriptCV.js
+++ assets/scriptCV.js
@@ -3,22 +3,39 @@ function init() {
     var btnPopup = document.getElementById('btnPopup');
     var overlay = document.getElementById('overlay');
     btnPopup.addEventListener('click', open);
-    function open() {
-        overlay.style.display = 'block';
-    }
-    
+
+
     var btnClose = document.getElementById('btnClose');
-    function close() {
-        overlay.style.display='none';
-    }
-    btnClose.addEventListener('click',close)
-    
+    btnClose.addEventListener('click', close)
+/*
     var nav = document.getElementById('blocTexte');
     var openn = true;
-    overlay.addEventListener('click',closeout);
+    overlay.addEventListener('click', closeout);
     function closeout() {
-        nav.addEventListener('click',function () {openn = true;})
-        if (!openn) overlay.style.display='none';
+        nav.addEventListener('click', function () { openn = true; })
+        if (!openn) overlay.style.display = 'none';
         openn = false;
+    }*/
+
+    var btnChat = document.getElementById('btnChat');
+    var overlay2 = document.getElementById('overlayy');
+    function openChat() {
+        overlay2.style.display = 'block';   
     }
+    btnChat.addEventListener('click', openChat);
+
+
+    var btnCloseChat = document.getElementById('closeChat');
+    function closeChat() {
+        overlay2.style.display = 'none';
+    }
+    btnCloseChat.addEventListener('click', closeChat)
 }
+
+function open() {
+    overlay.style.display = 'block';
+}
+function close() {
+    overlay.style.display = 'none';
+}
+
